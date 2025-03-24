aab4d21e562db1d4972c7f793ba0694a79114a89
Joachim Strömbergson <joachim@assured.se>
2025-03-24 14:46:59 +0100

    (fpga) Add initial version of all source files.
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- rtl/eim_poc.v
+++ rtl/eim_poc.v
@@ -30,36 +30,45 @@
 //
 //======================================================================
 
-module eim
+module eim_poc
   (
    // system clock and reset
-   input wire 	       clk,
-   input wire 	       reset_n,
+   input wire 	       clk48,
 
    // EIM interface pins from CPU
-   input wire 	       eim_bclk, // EIM burst clock. Started by the CPU.
-   input wire 	       eim_cs0_n, // Chip select (active low).
-   inout wire [7 : 0]  eim_da, // Bidirectional address and data port.
-   input wire 	       eim_lba_n, // Latch address signal (active low).
-   input wire 	       eim_wr_n, // write enable signal (active low).
-   input wire 	       eim_oe_n, // output enable signal (active low).
+   input wire 	       eim_bclk,   // EIM burst clock. Started by the CPU.
+   input wire 	       eim_cs0_n,  // Chip select (active low).
+   inout wire [7 : 0]  eim_da,     // Bidirectional address and data port.
+   input wire 	       eim_lba_n,  // Latch address signal (active low).
+   input wire 	       eim_wr_n,   // Write enable signal (active low).
+   input wire 	       eim_oe_n,   // Output enable signal (active low).
    output wire 	       eim_wait_n, // Data wait signal (active low).
 
    output wire [2 : 0]  led
    );
 
-   
-  //----------------------------------------------------------------
-  // eim_ctrl
-  //
-  // EIM arbiter handles EIM accesses and transfers it into
-  // `sys_clk' clock domain. It also implements the FSM part
-  // of the PoC. (This should probably bli separated.)
-  //----------------------------------------------------------------
-   eim_ctrl eim_ctrl_inst
-     (
-      .sys_clk(clk),
+  
+  //--------------------------------------------------------------------
+  // Generate clock and reset using PLL resources and trees.
+  //--------------------------------------------------------------------
+  wire sys_clk;
+  wire rst_n;
+  
+  clk_reset_gen clk_reset_geb_ins (
+                                   .clk_ref(clk48),
+                                   .clk(sys_clk),
+                                   .rst_n(rst_n)
+                                   );
 
+   //----------------------------------------------------------------
+   // EIM Arbiter
+   //
+   // EIM arbiter handles EIM access and transfers it into
+   // `sys_clk' clock domain.
+   //----------------------------------------------------------------
+
+   eim_arbiter eim_arbiter_inst
+     (
       .eim_bclk(eim_bclk),
       .eim_cs0_n(eim_cs0_n),
       .eim_da(eim_da),
@@ -69,12 +78,33 @@ module eim
       .eim_oe_n(eim_oe_n),
       .eim_wait_n(eim_wait_n),
 
-      .led(led)
+      .sys_clk(sys_clk),
+
+      .sys_addr(sys_eim_addr),
+      .sys_wren(sys_eim_wr),
+      .sys_data_out(sys_eim_dout),
+      .sys_rden(sys_eim_rd),
+      .sys_data_in(sys_eim_din)
       );
 
 
-endmodule // eim
+   //----------------------------------------------------------------
+   // LED Driver
+   //
+   // A simple utility LED driver that turns on the Novena
+   // board LED when the EIM interface is active.
+   //----------------------------------------------------------------
+   eim_indicator eim_indicator_inst
+     (
+      .sys_clk(sys_clk),
+      .sys_rst_n(rst_n),
+      .eim_active(sys_eim_wr | sys_eim_rd),
+      .led_out(led_pin)
+      );
+
+
+endmodule // eim_poc
 
 //======================================================================
-// EOF eim.v
+// EOF eim_poc.v
 //======================================================================
